bd6c5fdec7c1dc03068657667dc6a1eda96d89ba
grvsharma1810 <gs8280601@gmail.com>
2021-04-29 19:12:32 +0530

    fix: Cart UI and Card UI

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,47 +1,49 @@
-import './cart.css'
-import { useData } from '../../providers/DataProvider'
-import { CartCard } from './components/cart-card/CartCard'
-import { CartPrice } from './components/cart-price/CartPrice';
+import "./cart.css";
+import { useData } from "../../providers/DataProvider";
+import { CartCard } from "./components/cart-card/CartCard";
+import { CartPrice } from "./components/cart-price/CartPrice";
 
-const Cart = () => {            
-
-    const { dataState } = useData();    
+const Cart = () => {
+    const { dataState } = useData();
 
     const cart = dataState.cart ? dataState.cart : [];
 
     return (
-        <div>                      
-            {
-                cart.length > 0 &&
+        <div>
+            {cart.length > 0 && (
                 <div className="cart-container">
                     <div>
-                        <h4 className="text-size-2 text-heading-medium mb-1">MY CART</h4>
+                        <h4 className="text-size-2 text-heading-medium mb-1">
+                            MY CART
+                        </h4>
                         <div className="flex flex-column flex-gap-1">
-                            {
-                                cart.map((cartItem) => {
-                                    return (
-                                        <CartCard cartItem={cartItem} key={cartItem._id} />
-                                    )
-                                })
-                            }
+                            {cart.map((cartItem) => {
+                                return (
+                                    <CartCard
+                                        cartItem={cartItem}
+                                        key={cartItem._id}
+                                    />
+                                );
+                            })}
                         </div>
                     </div>
-                    <div>
-                        <h4 className="text-size-2 text-heading-medium">PRICE DETAILS</h4>
+                    <div className="cart-price">
+                        <h4 className="text-size-2 text-heading-medium">
+                            PRICE DETAILS
+                        </h4>
                         <CartPrice />
                     </div>
                 </div>
-            }
-            {
-                cart.length === 0 &&
+            )}
+            {cart.length === 0 && (
                 <div className="flex h-center p-1">
                     <p className="text-heading-medium text-size-3">
                         Your Cart Is Empty. Please Add Something In Cart.
                     </p>
                 </div>
-            }
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Cart;
